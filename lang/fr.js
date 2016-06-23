489f9b669b150154ef5967715c95d89427ef46ca
Road-Link <infos@sudtelematique.com>
2016-06-23 18:59:03 +0200

    Update fr.js

--- lang/fr.js
+++ lang/fr.js
@@ -1,15 +1,15 @@
 TRANSLATIONS = {
-	"Choose unit": "",
-	"Engine operation": "",
-	"Unit": "",
-	"Print": "",
-	"No ignition sensor.": "",
-	"Date": "",
-	"Interval": "",
-	"Yesterday": "",
-	"Today": "",
-	"Week": "",
-	"Month": "",
-	"Custom": "",
-	"ON time": ""
+	"Choose unit": "Choix Unité",
+	"Engine operation": "Temps de Fonctionnement",
+	"Unit": "Unité",
+	"Print": "Imprimer",
+	"No ignition sensor.": "Après-Contact OFF",
+	"Date": "Date",
+	"Interval": "Périodes",
+	"Yesterday": "Hier",
+	"Today": "Aujourd'hui",
+	"Week": "Semaine",
+	"Month": "Mois",
+	"Custom": "Personnaliser",
+	"ON time": "Cumul"
 };
